85971174a8a38526997a927e74d9b6ab9cd76e3b
Bomi <bkim1289@gmail.com>
2017-11-15 18:40:30 +0000

    Done.

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,3 +2,24 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
+IcebreakerResponse.BatchCreate = function(emails){
+  return emails.map(function(email){
+    new IcebreakerResponse(email);
+  })
+}
+/*IcebreakerResponse.BatchCreate = function(emails){
+  let icebreakers = [];
+
+  emails.forEach(function(email){
+    icebreakers.push(new IcebreakerResponse(email))
+  })
+
+  return icebreakers;
+}*/
+
+
+/*IcebreakerResponse.BatchCreate = function(data) {
+  return data.map(function(IcebreakerResponseEmail){
+    return new IcebreakerResponse()
+  })
+}*/
