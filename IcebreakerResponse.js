4375248b882135e7c44a302a6514170c9d421409
Bomi <bkim1289@gmail.com>
2017-11-16 04:47:30 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -4,7 +4,7 @@ function IcebreakerResponse(email){
 
 IcebreakerResponse.BatchCreate = function(emails){
   return emails.map(function(email){
-    new IcebreakerResponse(email);
+    return new IcebreakerResponse(email);
   })
 }
 /*IcebreakerResponse.BatchCreate = function(emails){
